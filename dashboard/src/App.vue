b466c109d8970eda4be5f42e44f7f03ed96fb8ab
Morgan Trotter <mhtrotte@ucsc.edu>
2021-03-05 13:23:20 -0800

    Named Slots

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -1,24 +1,29 @@
 <template>
-  <div id="app">
-    <HelloWorld msg="Dashboard for Paperchain" />
-    <AppButton @click="log">
-      <Icon />
-      add to favorites
-    </AppButton>
-  </div>
+  <AppLayout>
+    <template #header>Slots are awesome</template>
+    <template #default>
+      <HelloWorld msg="Dashboard for Paperchain" />
+      <AppButton @click="log">
+        <Icon />
+        add to favorites
+      </AppButton>
+    </template>
+  </AppLayout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import AppButton from './components/AppButton.vue'
-import Icon from './components/Icon.vue'
+import AppLayout from './components/AppLayout.vue';
+import HelloWorld from './components/HelloWorld.vue';
+import AppButton from './components/AppButton.vue';
+import Icon from './components/Icon.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
     AppButton,
-    Icon
+    Icon,
+    AppLayout
   },
   methods: {
     log() {
---
+++ dashboard/src/components/AppLayout.vue
@@ -0,0 +1,13 @@
+<template>
+	<div class="layout">
+		<header>
+			<slot name="header" />
+		</header>
+		<main>
+			<slot />
+		</main>
+		<footer>
+			<slot name="footer">made with slots</slot>
+		</footer>
+	</div>
+</template>
